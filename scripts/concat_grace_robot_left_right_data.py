d6abe9d4a5e81325d1cfbc6257396825a986e308
jpmnieles <jpmnieles@connect.ust.hk>
2024-09-27 04:49:38 +0800

    Added concat dataset for left and right eye with rvec and tvec
    Signed-off-by: jpmnieles <jpmnieles@connect.ust.hk>

---
+++ scripts/concat_grace_robot_left_right_data.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import glob
+import numpy as np
+import pandas as pd
+
+def preprocess_csv(data_dir, depth, eye):
+    # Read CSV
+    files_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..",'data', data_dir, f"*{depth}*", f"*{eye}*.csv")
+    csv_files = glob.glob(files_path)
+    
+    dfs = []
+    for csv_file in csv_files:
+        temp_df = pd.read_csv(csv_file)
+        dfs.append(temp_df)
+    df = pd.concat(dfs, ignore_index=True)
+
+    data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..','data','structured',f"075m_{eye}_concat.csv")
+    df.to_csv(data_path , index=False)
+    print('Saved to:',data_path)
+
+if __name__ == '__main__':
+    preprocess_csv('raw','d075','left')
+    preprocess_csv('raw','d075','right')
